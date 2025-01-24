1cddbd96eb31b4cbb3e3262b6ab62487df731119
codebykuldeep <kuldeep.pal@celestialsys.com>
2025-01-24 22:56:13 +0530

    home page

--- client/src/components/Common/Home/Home.tsx
+++ client/src/components/Common/Home/Home.tsx
@@ -1,24 +1,37 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { RootState } from '../../../store/store'
 import { getTokenString, redirectToDashboard } from '../../../utils/utilsFunctions';
+import classes from './home.module.css';
+import { Button } from '@mui/material';
 
 function Home() {
   const user  = useSelector((state:RootState)=>state.userSlice.user);
   const navigate = useNavigate();
+  const userStatus = getTokenString() && user;
  
-  useEffect(()=>{
-    if(getTokenString() && user){
+  function handleClick(){
+    if(userStatus){
       navigate(redirectToDashboard(user));
     }
-  },[user,navigate])
+    else{
+      navigate('/auth');
+    }
+    
+  }
   
   return (
-    <div>
-      <Link to={'/auth'}>LOGIN / REGISTER</Link>
+    <div className={classes.container}>
+      <div>
+        <div className={classes.text}>Unlock Your Future: Find Your <br /><strong>Perfect Job</strong> Today!</div>
+        <p className={classes.para}>Welcome to findJob, where your dream job awaits! Browse thousands of job listings.</p>
+        <div className={classes.button}><Button variant='contained' onClick={handleClick}>{userStatus ? "DashBoard" :'Login'}</Button></div>
+      </div>
     </div>
   )
 }
 
 export default Home
+
+
