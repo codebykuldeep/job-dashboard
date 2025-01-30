07269280383de9b6e995b40b2cecddf48404d112
codebykuldeep <kuldeep.pal@celestialsys.com>
2025-01-30 11:17:58 +0530

    rules update

--- client/src/utils/validation.ts
+++ client/src/utils/validation.ts
@@ -1,6 +1,19 @@
 import { FormStateType } from "../types/formTypes";
 
+function nameValidation(value:string):[string,boolean]{
+    if(value.trim()===''){
+        return ['This field is required',true];
+    }
 
+    const pattern = /^[a-zA-Z ]*$/;
+    if(!pattern.test(value)){
+        return ['Enter a valid name',true];
+    }
+    else if(value.length <3){
+        return ['Enter a name of valid length',true];
+    }
+    return ['',false];
+}
 function emailValidation(value:string):[string,boolean]{
     if(value.trim()===''){
         return ['This field is required',true];
@@ -32,6 +45,9 @@ function fieldValidation(value:string):[string,boolean]{
     
     if(String(value).trim() ===''){
         return ['This field is required',true];
+    }
+    else if(String(value).length < 3){
+        return ['Please enter valid data',true];
     }
     
     return ['',false];
@@ -106,6 +122,9 @@ export function validation(title:string,value:string):[string,boolean]{
             return [`Please enter valid experience years ranging (0 - 15)`,true];
         }
         return ['',false];
+    }
+    if(title === 'name' ){
+        return nameValidation(value);
     }
 
     if(title === 'date'){
